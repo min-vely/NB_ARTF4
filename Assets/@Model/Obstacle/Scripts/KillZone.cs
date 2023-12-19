412f5ae50895d676469cea3c7b469f5cf0a9cb38
Daniel Lee <jhoon8903@gmail.com>
2023-12-19 10:23:32 +0900

    [👨🏻‍🚒 HOTFIX]

--- Assets/@Model/Obstacle/Scripts/KillZone.cs
+++ Assets/@Model/Obstacle/Scripts/KillZone.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class KillZone : MonoBehaviour
 {
+
+    public static event Action OnDeath;
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
 			//col.gameObject.GetComponent<CharacterControls>().LoadCheckPoint();
             col.gameObject.GetComponent<PlayerController>().LoadCheckPoint();
+            OnDeath?.Invoke();
         }
 	}
 }
