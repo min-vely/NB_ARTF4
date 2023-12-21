b37dbca9b6903276b9a050a5e06baff1adf820df
Daniel Lee <jhoon8903@gmail.com>
2023-12-21 12:09:03 +0900

    [🛠️ Fix] Mass Fix
    Mass fix

--- Assets/@Scripts/Scene/GameScene.cs
+++ Assets/@Scripts/Scene/GameScene.cs
@@ -1,6 +1,5 @@
 using Scripts.Event;
 using UnityEngine;
-
 namespace Scripts.Scene
 {
     public class GameScene : BaseScene
@@ -10,9 +9,11 @@ namespace Scripts.Scene
             if (!base.Initialized()) return false;
             Main.SetCurrentScene(this, Label.GameScene);
             GameSetup();
+            AudioClip clip = Main.Resource.Load<AudioClip>("GameBGM1.clip");
+            Main.Sound.StartBGM();
+            Main.Sound.PlayBGM(clip.name);
             return true;
         }
-
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape) && !Main.UI.IsPausePanelOpen) // ESC 누르면 Pause
@@ -20,28 +21,24 @@ namespace Scripts.Scene
                 OpenPausePopUp();
             }
         }
-
         private void GameSetup()
         {
-            Main.UI.SetSceneUI<Game_UI>(); 
+            Main.UI.SetSceneUI<Game_UI>();
             SubscribeEvent();
             Main.Obstacle.Initialized();
             Main.Game.SpawnPlayer();
             Main.Item.Initialized();
         }
-
         private void SubscribeEvent()
         {
             KillZone.OnDeath -= OpenDeathPopUp;
             KillZone.OnDeath += OpenDeathPopUp;
         }
-
         private void OpenDeathPopUp()
         {
             Main.UI.OpenPopup<DiePanel_Popup>();
             Main.PlayerControl.ToggleCursor(true); // 커서 잠금해제
         }
-
         private void OpenPausePopUp()
         {
             Main.UI.OpenPopup<PausePanel_Popup>();
